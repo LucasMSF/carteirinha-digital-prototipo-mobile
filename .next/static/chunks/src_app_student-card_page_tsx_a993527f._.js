33be4e0b4bb7fab632a233fe1944aa03ec44e557
Lucas Mathias <89022473+LucasMSF@users.noreply.github.com>
2025-08-24 00:02:17 -0300

    Add .next/static/chunks/src_app_student-card_page_tsx_a993527f._.js 1756004537578

---
+++ .next/static/chunks/src_app_student-card_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_a0629328._.js",
+  "static/chunks/src_e4514bea._.js"
+],
+    source: "dynamic"
+});
